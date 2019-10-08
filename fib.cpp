0e5a99691c4848069c61e0af9c7ededdf825a295
Vishal <vishal19102@iiitd.ac.in>
2019-10-08 23:19:44 +0530

    Fib space optimized

---
+++ fib.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n,a,b,c;
+    cin>>n;
+    a=0;
+    b=1;
+    for(int i=2;i<=n;i++){
+        c = a+b;
+        a=b;
+        b=c;
+    }
+    cout<<c<<endl;
+    return 0;
+}
